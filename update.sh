be2eed8cabeafe0f7cfa2ed954d98a7cdbd202c7
King-1025 <1543641386@qq.com>
2018-11-03 00:19:15 +0800

    2018-11-03 00:19:15

--- update.sh
+++ update.sh
@@ -39,6 +39,8 @@ function update_by_doubiSSR()
     else
       make_data "$SSR_FILE" "$ROOT/$SUBSCRIBE" "$REMARKS" "$GROUP"
     fi
+    echo "update ESC..."
+    sf put "$ROOT/$SUBSCRIBE"
     fresh
   fi
 }
